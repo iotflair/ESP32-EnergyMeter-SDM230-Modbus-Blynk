ffe651c74ed159279a22da0f39e2fa6224a82a34
iotflair <37087557+iotflair@users.noreply.github.com>
2018-09-27 23:57:42 +0300

    Update ESP32-EnergyMeter-SDM230-Modbus-Blynk.ino

--- ESP32-EnergyMeter-SDM230-Modbus-Blynk.ino
+++ ESP32-EnergyMeter-SDM230-Modbus-Blynk.ino
@@ -1,20 +1,16 @@
-/* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
-
 
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
-#include <HTTPClient.h>
 #include "SDM.h"                                                               //import SDM library
 
 HardwareSerial SerialSDM(1);                                                   //Set Hardware Serial(pins 16 & 17)
-SDM sdm(SerialSDM, 2400);                                                      //config SDM
+SDM sdm(SerialSDM, 2400);   
 
 float voltage,current,power,frequency,energy;
 
 // You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
 char auth[] = "AUTH";
 
 // Your WiFi credentials.
@@ -23,44 +19,110 @@ char ssid[] = "SSID";
 char pass[] = "PASS";
 
 void sdm_get(void);
-
+bool triggerBlynkConnect = false;
+bool isFirstConnect = true; // Keep this flag not to re-sync on every reconnection
 BlynkTimer timer;
 
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
 void myTimerEvent()
 {
-  sdm_get();
-  Blynk.virtualWrite(V5, voltage);
-  Blynk.virtualWrite(V6, current);
-  Blynk.virtualWrite(V7, power);
-  Blynk.virtualWrite(V8, frequency);
-  Blynk.virtualWrite(V9, energy);
-}
-
-
-void setup()
-{
-  // Debug console
-  Serial.begin(9600);
-  sdm.begin(); 
-
-  Blynk.begin(auth, ssid, pass);
-
-  myTimerEvent();
-  // Setup a function to be called every 10 minutes
-  timer.setInterval(600000L, myTimerEvent);
-}
-
-void loop()
-{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
   if (WiFi.status() != WL_CONNECTED)
   {
-      WiFi.reconnect();
-      //Serial.print("Reconnecting....");
-      delay(5000);
+    Serial.println(F("WiFi not connected"));
+    
+    Blynk.connectWiFi(ssid, pass);
   }
-  Blynk.run();
+  else
+  {
+    Serial.println(F("WiFi in connected"));
+  }
+
+  if (!Blynk.connected() && WiFi.status() == WL_CONNECTED)
+  {
+    if (Blynk.connect())
+    {
+      Serial.println(F("Blynk reconnected"));
+    }
+    else
+    {
+      Serial.println(F("Blynk not reconnected"));
+    }
+  }
+  else
+  {
+    Serial.println(F("Blynk connected"));
+  }
+
+  if (Blynk.connected() && WiFi.status() == WL_CONNECTED)
+  {
+    sdm_get();
+    Blynk.virtualWrite(V5, voltage);
+    Blynk.virtualWrite(V6, current);
+    Blynk.virtualWrite(V7, power);
+    Blynk.virtualWrite(V8, frequency);
+    Blynk.virtualWrite(V9, energy);
+
+  }
+}
+
+BLYNK_CONNECTED()
+{
+  triggerBlynkConnect = true;
+
+  Serial.println(F("Blynk Connected!"));
+  Serial.println(F("local ip"));
+  Serial.println(WiFi.localIP());
+
+  if (isFirstConnect)
+  {       
+    Blynk.syncAll();    
+
+    isFirstConnect = false;
+  }
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  sdm.begin(); 
+  
+  Blynk.config(auth);
+  Blynk.connectWiFi(ssid, pass);
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    if (Blynk.connect())
+    {
+      triggerBlynkConnect = true;
+    }
+    else
+    {
+      triggerBlynkConnect = false;
+    }
+  }
+
+  myTimerEvent();
+  timer.setInterval(600000L, myTimerEvent);       //Every 10 minutes
+}
+void loop() {
+
+  if (Blynk.connected())
+  {
+    Blynk.run(); // Initiates Blynk Server  
+  }
+  else
+  {
+    triggerBlynkConnect = false;
+  }
+  
   timer.run(); // Initiates BlynkTimer 
 }
+
 
 //************************* SDM230  ******************************//
 void sdm_get(){ 
